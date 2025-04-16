27b6c1f46e194520d7174b547358e2224f9c468a
Damien <damien.rat44@gmail.com>
2025-04-16 10:24:39 +0200

    Initialise spring boot project

---
+++ src/main/java/formation_sopra/Refuge/RefugeApplication.java
@@ -0,0 +1,13 @@
+package formation_sopra.Refuge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefugeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefugeApplication.class, args);
+	}
+
+}
