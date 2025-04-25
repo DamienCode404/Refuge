00826a03d096b9f7ee3f993b30e6885af958ac95
Adrien <adrienmalet@orange.fr>
2025-04-25 16:37:06 +0200

    Modif AuthService pour récup le user connecté (id + role) ET idWorker pour animaux

--- src/main/java/formation_sopra/Refuge/rest/response/ConnexionResponse.java
+++ src/main/java/formation_sopra/Refuge/rest/response/ConnexionResponse.java
@@ -2,7 +2,9 @@ package formation_sopra.Refuge.rest.response;
 
 public class ConnexionResponse {
 	private boolean success;
-	private String token; // ← Ajout de la propriété token
+	private String token;// ← Ajout de la propriété token
+	private Integer idUser;
+	private String roleUser;
 
 	public ConnexionResponse() {
 		super();
@@ -23,4 +25,23 @@ public class ConnexionResponse {
 	public void setToken(String token) {
 		this.token = token;
 	}
+
+	public Integer getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getRoleUser() {
+		return roleUser;
+	}
+
+	public void setRoleUser(String roleUser) {
+		this.roleUser = roleUser;
+	}
+	
+	
+	
 }
